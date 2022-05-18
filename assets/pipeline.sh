68c236f92cd8aba9293da545b3a54fbedf5a290e
jw320 <jw320@lab-rg07-06.cms.waikato.ac.nz>
2022-05-18 16:34:32 +1200

    Test

---
+++ assets/pipeline.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ $# -eq 1 ]]
+then
+  #npm install
+  if npm run build
+  then
+    git add .
+    git commit -m "$1"
+    git push .
+    #npm run start
+  else
+    echo "Build did not compile"
+  fi
+else
+  echo "One commit comment required"
+fi
